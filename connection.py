19f9543a68df374a8f66f2c63f1ec83760617e83
inforticx <71990929+inforticx@users.noreply.github.com>
2021-09-29 16:52:43 +0400

    Test BigQuery connection with python

---
+++ connection.py
@@ -0,0 +1,23 @@
+## Test connection from python3 to BigQuery
+
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+query = """
+    SELECT name, SUM(number) as total_people
+    FROM `bigquery-public-data.usa_names.usa_1910_2013`
+    WHERE state = 'TX'
+    GROUP BY name, state
+    ORDER BY total_people DESC
+    LIMIT 20
+"""
+query_job = client.query(query)  # Make an API request.
+
+print("The query data:")
+for row in query_job:
+    print("name={}, count={}".format(row[0], row["total_people"]))
+    
+    
+    
